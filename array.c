ff4d867d45bcedbd08e650e80a604ba2a85a3842
SiddojuRakesh <rakesh.siddoju@thundersoft.com>
2022-02-01 08:22:51 +0530

    this is array file

---
+++ array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int array[4],i,sum=0;
+for(i=0;i<4;i++)
+{
+printf("enter the value for array[%d]: ",i);
+scanf("%d",&array[i]);
+
+sum=sum+array[i];
+}
+printf("sum=%d",sum);
+return 0;
+}
---
+++ file.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+printf("hello rakesh");
+return 0;
+}
+
